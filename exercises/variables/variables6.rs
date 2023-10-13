a2ea4d01748508fb0a40e1e69d6a408c830fd2e3
cpx <12119342+carpedil@users.noreply.github.com>
2023-10-13 11:12:09 +0800

    常量的声明

--- exercises/variables/variables6.rs
+++ exercises/variables/variables6.rs
@@ -3,9 +3,8 @@
 // Execute `rustlings hint variables6` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
-const NUMBER = 3;
+// 常量在声明的时候,必须标注类型和值
+const NUMBER: i32 = 3;
 fn main() {
     println!("Number {}", NUMBER);
 }
