6d7565c23df29d108981c9fa3ed20bb600fb174c
cpx <12119342+carpedil@users.noreply.github.com>
2023-10-13 11:29:56 +0800

    函数返回值

--- exercises/functions/functions4.rs
+++ exercises/functions/functions4.rs
@@ -8,14 +8,13 @@
 // Execute `rustlings hint functions4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
     let original_price = 51;
     println!("Your sale price is {}", sale_price(original_price));
 }
-
-fn sale_price(price: i32) -> {
+// 返回值类型需要声明, 如果没有声明具体返回值类型, 默认返回()类型
+fn sale_price(price: i32) -> i32 {
     if is_even(price) {
         price - 10
     } else {
