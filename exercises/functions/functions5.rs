3b41154ffa526098a18c40127e8092fd59568921
cpx <12119342+carpedil@users.noreply.github.com>
2023-10-13 11:35:25 +0800

    函数返回值, 语句与表达式的区别

--- exercises/functions/functions5.rs
+++ exercises/functions/functions5.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint functions5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
     let answer = square(3);
@@ -11,5 +10,9 @@ fn main() {
 }
 
 fn square(num: i32) -> i32 {
-    num * num;
+    // 在rust中以分号;结尾的被称为语句, 不能当作类型进行返回, 表达式可以作为返回值返回
+    // 因为表达式最终都会有个一结果, 这个结果一定有对应的类型, 如果
+    // 表达式的结果与函数的返回值类型一致, 就可以直接返回对应的表达式即可
+    // num * num;
+    num * num
 }
