18fe03e99668045fc5476beb1f0e66280b2d06f7
cpx <12119342+carpedil@users.noreply.github.com>
2023-10-13 15:39:30 +0800

    pub关键字与模块可见性

--- exercises/modules/modules1.rs
+++ exercises/modules/modules1.rs
@@ -3,20 +3,22 @@
 // Execute `rustlings hint modules1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
+// 1. 此处sausage_factory模块相对于当前modules1.rs而言属于其内部模块
 mod sausage_factory {
     // Don't let anybody outside of this module see this!
     fn get_secret_recipe() -> String {
         String::from("Ginger")
     }
-
-    fn make_sausage() {
+    // 3. 添加pub关键字可以让当前函数或方法在外部module中可见
+    pub fn make_sausage() {
         get_secret_recipe();
         println!("sausage!");
     }
 }
 
 fn main() {
+    // 2. 所以在main()函数中调用sausage_factory模块的make_sausage函数的时候, 必须确保
+    // make_sausage函数对父模块/外部模块可见
     sausage_factory::make_sausage();
 }
