dbeb3e5b12de0cc0189ce29722c8273b2c6afd92
cpx <12119342+carpedil@users.noreply.github.com>
2023-10-13 11:18:10 +0800

    函数参数类型

--- exercises/functions/functions2.rs
+++ exercises/functions/functions2.rs
@@ -3,13 +3,18 @@
 // Execute `rustlings hint functions2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
+    // rust可以自动根据函数call_me的形参进行自动推断变量的类型
     call_me(3);
+    // 等价于cell_me(8)
+    call_me(8u32);
+    // 类型就不匹配, 调用会报错
+    // let n = 8i8;
+    // call_me(n);
 }
 
-fn call_me(num:) {
+fn call_me(num:u32) {
     for i in 0..num {
         println!("Ring! Call number {}", i + 1);
     }
