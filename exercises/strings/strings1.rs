784d29a8c373567e02a341ae9e8f4eafba3f1d57
cpx <12119342+carpedil@users.noreply.github.com>
2023-10-13 15:15:07 +0800

    str.to_string()

--- exercises/strings/strings1.rs
+++ exercises/strings/strings1.rs
@@ -5,7 +5,6 @@
 // Execute `rustlings hint strings1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
     let answer = current_favorite_color();
@@ -13,5 +12,7 @@ fn main() {
 }
 
 fn current_favorite_color() -> String {
-    "blue"
+    // 用双引号包裹的比如"hello"在rust中被称为&str, 不是String类型,
+    // 但是&str可以通过调用.to_string()方法转换成String类型
+    "blue".to_string()
 }
