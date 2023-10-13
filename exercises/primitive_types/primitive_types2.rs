04a3a3b752c74c326e889687645b1b0eabc65e10
cpx <12119342+carpedil@users.noreply.github.com>
2023-10-13 12:00:46 +0800

    char 类型

--- exercises/primitive_types/primitive_types2.rs
+++ exercises/primitive_types/primitive_types2.rs
@@ -3,13 +3,14 @@
 // Fill in the rest of the line that has code missing! No hints, there's no
 // tricks, just get used to typing these :)
 
-// I AM NOT DONE
 
 fn main() {
     // Characters (`char`)
 
     // Note the _single_ quotes, these are different from the double quotes
     // you've been seeing around.
+    // 使用单引号包裹的在rust中被认为是char类型, 并且单引号中只能是单字符,不能是'2b',或者'sb'
+    // 但是可以是'🙂'emoji类型
     let my_first_initial = 'C';
     if my_first_initial.is_alphabetic() {
         println!("Alphabetical!");
@@ -19,7 +20,7 @@ fn main() {
         println!("Neither alphabetic nor numeric!");
     }
 
-    let // Finish this line like the example! What's your favorite character?
+    let your_character = '🙂'; // Finish this line like the example! What's your favorite character?
     // Try a letter, try a number, try a special character, try a character
     // from a different language than your own, try an emoji!
     if your_character.is_alphabetic() {
