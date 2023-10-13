baf0c4a67c404dc4da228d5e3a975ed9ac358c97
cpx <12119342+carpedil@users.noreply.github.com>
2023-10-13 15:21:41 +0800

    &str入参

--- exercises/strings/strings2.rs
+++ exercises/strings/strings2.rs
@@ -5,17 +5,20 @@
 // Execute `rustlings hint strings2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
+    // 2. 但是此处声明的word是一个字符串String类型, 且此处不允许修改word的类型
     let word = String::from("green"); // Try not changing this line :)
-    if is_a_color_word(word) {
+    // 3. 所以在调用is_a_color_word函数时, 可以在word前面添加&符号即可
+    // 为什么? 待研究TODO
+    if is_a_color_word(&word) {
         println!("That is a color word I know!");
     } else {
         println!("That is not a color word I know.");
     }
 }
 
+// 1. is_a_color_word 函数要求传入的attempt是一个&str类型
 fn is_a_color_word(attempt: &str) -> bool {
     attempt == "green" || attempt == "blue" || attempt == "red"
 }
