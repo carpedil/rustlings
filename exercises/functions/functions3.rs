be39e9132acea0de0134f1cb5c2696c8713434bc
cpx <12119342+carpedil@users.noreply.github.com>
2023-10-13 11:19:39 +0800

    函数调用和参数传递

--- exercises/functions/functions3.rs
+++ exercises/functions/functions3.rs
@@ -3,10 +3,10 @@
 // Execute `rustlings hint functions3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
-    call_me();
+    // 如果函数定义的时候声明了参数, 在调用的时候必须参照声明传递实际的参数
+    call_me(83);
 }
 
 fn call_me(num: u32) {
