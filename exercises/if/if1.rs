976da6f81957804480e46778a5f228eaad64973b
cpx <12119342+carpedil@users.noreply.github.com>
2023-10-13 11:39:43 +0800

    使用if-else实现三目运算

--- exercises/if/if1.rs
+++ exercises/if/if1.rs
@@ -2,13 +2,15 @@
 //
 // Execute `rustlings hint if1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 pub fn bigger(a: i32, b: i32) -> i32 {
     // Complete this function to return the bigger number!
     // Do not use:
     // - another function call
     // - additional variables
+    // 类似于js中的三目运算 a > b ? a : b
+    // 此处不需要在结尾添加;号
+    if a > b {a} else {b}
 }
 
 // Don't mind this for now :)
