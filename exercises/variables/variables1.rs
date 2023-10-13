2f622afd48aa9801aa10c58bffae10f1d28686f8
cpx <12119342+carpedil@users.noreply.github.com>
2023-10-13 11:05:23 +0800

    fix: let missing

--- exercises/variables/variables1.rs
+++ exercises/variables/variables1.rs
@@ -5,9 +5,9 @@
 // Execute `rustlings hint variables1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
-    x = 5;
+    // variable declearatin use let key word
+    let x = 5;
     println!("x has the value {}", x);
 }
