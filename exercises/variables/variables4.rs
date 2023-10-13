9d07debb60c428008d6e5001533d35e793fb0314
cpx <12119342+carpedil@users.noreply.github.com>
2023-10-13 11:09:32 +0800

    变量可变性mut使用

--- exercises/variables/variables4.rs
+++ exercises/variables/variables4.rs
@@ -3,10 +3,10 @@
 // Execute `rustlings hint variables4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let x = 3;
+    // 如果需要修改变量的值, 需要添加mut关键字进行声明
+    let mut x = 3;
     println!("Number {}", x);
     x = 5; // don't change this line
     println!("Number {}", x);
