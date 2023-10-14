ef687b3bde6ec234593108e9ecdd2a6890fd26c8
cpx <12119342+carpedil@users.noreply.github.com>
2023-10-14 17:15:14 +0800

    assert_eq

--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -6,12 +6,14 @@
 // Execute `rustlings hint tests2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        assert_eq!(1, 1);
+        assert_eq!(true, 2 > 1);
+        assert_eq!("hello", "hello");
     }
 }
