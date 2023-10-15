bb69c83fda2ede63787516bcdcc275d1e9392622
cpx <12119342+carpedil@users.noreply.github.com>
2023-10-15 17:53:25 +0800

    macros2

--- exercises/macros/macros2.rs
+++ exercises/macros/macros2.rs
@@ -3,14 +3,16 @@
 // Execute `rustlings hint macros2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-fn main() {
-    my_macro!();
-}
-
+// 跟函数不一样, 在单个文件里面, 宏需要先定义,才能在之后使用
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
     };
 }
+
+fn main() {
+    my_macro!();
+}
+
+
